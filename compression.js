ba8d4f623a5e8a6647f623fb24a1ff4272ae57a9
Durdaking <Jamesimcglade2@gmail.com>
2026-03-01 22:20:17 -0500

    Update compression.js

--- compression.js
+++ compression.js
@@ -1,1 +1,6 @@
-
+function compress(data){
+return btoa(unescape(encodeURIComponent(JSON.stringify(data))));
+}
+function decompress(str){
+return JSON.parse(decodeURIComponent(escape(atob(str))));
+}
