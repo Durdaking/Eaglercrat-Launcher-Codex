14a0ccc79da9d0c05f1c5984adae0252f6413843
Durdaking <Jamesimcglade2@gmail.com>
2026-03-02 06:38:00 -0500

    Delete compression.js

--- compression.js
+++
@@ -1,6 +0,0 @@
-function compress(data){
-return btoa(unescape(encodeURIComponent(JSON.stringify(data))));
-}
-function decompress(str){
-return JSON.parse(decodeURIComponent(escape(atob(str))));
-}
